97b1d428d68d71da0fe43f3226f50c91a1048768
Yanina Charas <ycharas@gmail.com>
2023-01-31 09:30:48 +0200

    agregue el contador al carrito y el uso de UseEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 
 import Header from "./components/Header/Header";
+import ItemCount from "./components/ItemCount/ItemCount";
 import ItemListContainer from "./components/ItemsListContainer/ItemListContainer";
 import Layout from "./components/Layout/Layout";
+import UseEffect from "./components/UseEffect/UseEffect";
 
 function App() {
 let nombreUsuario = "Yanina"
+
+const onAdd = ( cantidad )=>{
+  console.log(`se agregaron al carrito ${cantidad} unidades`)
+}
 
   return (
     <div className="App">
       <Layout>
         <Header/>  
         <ItemListContainer usuario={nombreUsuario} apellido="Charas" edad={51} />
+        <ItemCount stock={10} initial={2} onAdd={onAdd} />
+        <UseEffect />
       </Layout>
     </div>
   );
